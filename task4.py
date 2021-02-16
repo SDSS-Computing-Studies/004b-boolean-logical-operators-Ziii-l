89210638d5c480b6a60b69564472b36562cdf9dd
Ziii-l <ziweil101@deltalearns.ca>
2021-02-16 14:13:12 -0800

    Update task4.py

--- task4.py
+++ task4.py
@@ -20,9 +20,9 @@ You are not a VIP.
 """
 
 VIPNames = ("Guile","Blanka","Christine","Carol","Richard","Daniel","Chun-Li")
-name = input("please enter your name")
+name = str(input("please enter your name"))
 
-if name == VIPNames:
-    print("Hi"+" "+str(name)+"!"+" "+ "You are a VIP!")
+if name in VIPNames:
+    print("Hi"+" "+name+"! You are a VIP!")
 else:
     print("You are not a VIP.")

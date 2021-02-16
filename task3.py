d2a779e4b2ba0471a369dfdff301f1d6ba4d8d94
Ziii-l <ziweil101@deltalearns.ca>
2021-02-16 13:40:16 -0800

    Update task3.py

--- task3.py
+++ task3.py
@@ -17,9 +17,7 @@ Enter a number: -3
 """
 number = input("please enter a number")
 number=float(number)
-a= (number>=0)
-
-if number==int(number) and number>=a:
+if number==int(number) and number>=0:
     print(str(number)+"is a positive integer.")
 else:
     print(str(number)+ "is not a positive integer")
